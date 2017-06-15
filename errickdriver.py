7157fd4e0a0b044e117c7fb726644e0683e42b31
user <errick524@gmail.com>
2017-06-15 18:29:02 -0400

    began remaking trigger controls.

--- errickdriver.py
+++ errickdriver.py
@@ -2,39 +2,43 @@
 # coding: utf-8
 
 import rospy
-import serial
 import tf.transformations
 import Joy
-from geometry_msgs.msg
+from geometry_msgs.msg import Twist
 #reverse: address 128 motor 2 and address 129 motor 2
-address1 = 129
-address2 = 128
 #function to scale values in one range to values in another range -  proportional scaling
 def pscale(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
 
-def motor1speed(address,speed):
-    port = serial.Serial('/dev/ttyUSB0')
-    if(speed > 127):
-        speed = 127
-    if(speed < 0):
-        speed = 0
-    port.isOpen()
-    command1 = 6
-    command2 = 7
-    checksum1 = (address + command1 + speed) & 127
-    checksum2 = (address + command2 + pscale(speed,0,127,127,0)) & 127
-    #print("Left Speed: " + str(speed))
-    port.write("".join(chr(i) for i in [address, 14, 10, (address + 24) & 127]))
-    port.write("".join(chr(i) for i in [address, command1, speed, checksum1]))
-    port.write("".join(chr(i) for i in [address, command2, pscale(speed,0,127,127,0), checksum2]))
-
 def callback(msg):
+	if(msg.axes[5] <= 0.97):
+		linear = msg.axes[5]
+	elif(msg.axes[2] <= 0.97):
+		linear = msg.axes[2]
+	
+	angular = msg.axes[0]
+
+	if(angular < 0):
+		#right is negative
+		v1 = linear - angular
+        	v2 = linear
+	elif(angular > 0):
+		#left is positive
+		v1 = linear
+		v2 = linear - angular
+
+	if(v1 > 127) {
+		temp = v1 - 127
+		v2 = v2 + temp
+		v1 = 127
+	}
+	if(v1 < -127)
+		temp = 
 
 
 def listener():
 
-    rospy.init_node('sabertooth_driver') #anonymous=true would allow us to run multiple of these motor interpreters at once
+    rospy.init_node('TriggerJoystick') #anonymous=true would allow us to run multiple of these motor interpreters at once
 
     rospy.Subscriber("zenith/cmd_vel", Twist, callback,queue_size = 2)
     #rospy.Subscriber("cmd_vel",Twist,callback,queue_size = 2)
